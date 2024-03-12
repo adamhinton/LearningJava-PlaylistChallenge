3ffa2fdd6422d0193e39be983dbcc036645376ca
adamd <adam.d.hinton@gmail.com>
2024-03-12 10:58:45 -0400

    Copy in pre-made code

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,83 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+package dev.lpa;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    }
+}
+
+
+class Album {
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        songs = new ArrayList<>();
+    }
+    public boolean addSong(String title, double duration) {
+        if (findSong(title) == null) {
+            songs.add(new Song(title, duration));
+            return true;
         }
+        return false;
+    }
+
+    private Song findSong(String title) {
+        for (Song song : songs) {
+            if (song.getTitle().equals(title)) {
+                return song;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean addToPlayList(int trackNo, LinkedList<Song> playList) {
+        if (trackNo < 1 || trackNo > songs.size()) {
+            return false;
+        }
+        Song song = songs.get(trackNo - 1);
+        playList.add(song);
+        return true;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playList) {
+        Song song = findSong(title);
+        if (song != null) {
+            playList.add(song);
+            return true;
+        }
+
+        return false;
+    }
+}
+
+
+
+
+
+
+class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return title + ": " + duration;
     }
 }
